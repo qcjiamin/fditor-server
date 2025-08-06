51638434ef20719097b55659bab96cd985ced112
qc <qiuchen610714354@gmail.com>
2025-08-06 18:09:42 +0800

    feat: 添加启用端口测试 nginx 的负载均衡

---
+++ src/server1.ts
@@ -0,0 +1,14 @@
+import app from './app.js'
+import errorHandler from 'errorhandler'
+
+const PORT = process.env.PORT || 3060
+
+// 返回请求错误信息
+if (process.env.NODE_ENV === 'development') {
+  // only use in development
+  app.use(errorHandler())
+}
+
+app.listen(3091, '0.0.0.0', ()=>{
+    console.log(`已启动服务器： http://localhost:3091`);
+})
