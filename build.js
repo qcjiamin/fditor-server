28334ee6ba271b97370efa1e637ba1ea44c804ca
qc <qiuchen610714354@gmail.com>
2025-08-14 10:20:10 +0800

    feat: 明确路径问题，修改脚本

--- build.js
+++ build.js
@@ -194,8 +194,8 @@ const remoteDeploy = () => {
     // 创建工程目录，-p 如果有不会报错
     `mkdir -p ~/deploy`,
     // rm -rf 中的 -r 表示递归删除，-f 表示强制删除（不提示确认）
-    `rm -rf ~/deploy/*`,
-    `git clone https://github.com/qcjiamin/fditor-server.git ~/deploy`,
+    `rm -rf ~/deploy/fditor-server`,
+    `git clone https://github.com/qcjiamin/fditor-server.git ~/deploy/fditor-server`,
     `cd ~/deploy/fditor-server && docker-compose up -d --build`,
     // 移除悬空（无标签）镜像 -a 删除未被容器使用的镜像
     `docker image prune -f`,
